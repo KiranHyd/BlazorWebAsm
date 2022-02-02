d5b89104a86279e9d6adc64e098f9d730345af93
TeamLearnings <kiran9866650520@outlook.com>
2022-02-02 23:34:25 +0530

    Updating the project.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Blazor_PWA
+namespace BlazorWebAsm
 {
     public class Program
     {
@@ -18,13 +18,6 @@ namespace Blazor_PWA
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                // Configure your authentication provider options here.
-                // For more information, see https://aka.ms/blazor-standalone-auth
-                builder.Configuration.Bind("oidc", options.ProviderOptions);
-            });
 
             await builder.Build().RunAsync();
         }
